81c50c47360c5218cae88813ebe88451d75ab402
name <salmanmgr811201@gmail.com>
2025-06-27 22:59:32 +0530

    Adding file, images and components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react'
+// import { Navbar } from './components/Navbar'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div className='w-full overflow-hidden'>
+      {/* <Navbar /> */}
+      <Header />
+    </div>
+  )
+}
+
+export default App
